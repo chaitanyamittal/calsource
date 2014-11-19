14efd2384f22bfc6bc970ae70f444ab4bfdc7103
Sidharth Goel <sidharthgoel@airbears2-10-142-70-61.AirBears2.1918.Berkeley.EDU>
2014-11-18 17:33:59 -0800

    initialized

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119012859) do
+ActiveRecord::Schema.define(version: 20141119013309) do
+
+  create_table "projects", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
